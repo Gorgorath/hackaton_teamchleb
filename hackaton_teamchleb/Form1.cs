8a28749e2ef7c761fc960a5fe794c919c10ae215
ChrisKJM <chriskjm0903@gmail.com>
2020-07-06 16:12:37 +0200

    Zmieniono zmienną offset i dodano komentarze

--- hackaton_teamchleb/Form1.cs
+++ hackaton_teamchleb/Form1.cs
@@ -21,6 +21,7 @@ namespace hackaton_teamchleb
             InitializeComponent();
         }
 
+        //Przy kliknięciu guzika
         private void code_button_Click(object sender, EventArgs e)
         {
             input = input_textbox.Text.ToUpper();
@@ -30,14 +31,16 @@ namespace hackaton_teamchleb
             //Plugboard pboard = new Plugboard(new List<Plug> { tplug });
             //EntryWheel ew = new EntryWheel();
             //Reflector reflector = new Reflector("B");
-            //output_textbox.Text = reflector.ZamienZnak(inputChars[0]).ToString();
+            //output_textbox.Text = rotorL.ZamienZnak(inputChars[0]).ToString();
         }
     }
 
     class Plug
     {
+        //Plugi przechowują informacje na temat jaka litera ma być zastąpiona jaką inną i wice versa
         public char litera1, litera2;
 
+        //Konstruktor
         public Plug(char ch1, char ch2)
         {
             litera1 = ch1;
@@ -47,13 +50,16 @@ namespace hackaton_teamchleb
 
     class Plugboard
     {
+        //Przechowuje dane o plugach i zmienia znak zgodnie z nimi
         List<Plug> plugi = new List<Plug>();
 
+        //Konstruktor
         public Plugboard(List<Plug> pl)
         {
             plugi = pl;
         }
 
+        //Zmienia znak zgodnie z plugami
         public char ZamienZnak(char ch)
         {
             foreach (Plug plug in plugi)
@@ -73,6 +79,7 @@ namespace hackaton_teamchleb
 
     class Rotor
     {
+        //Serce enigmy. Rotory obracają się zmieniając za każdym razem literę końcową
         public int rotorPos;
         int ringPos;
         int offset;
@@ -82,12 +89,13 @@ namespace hackaton_teamchleb
         string znaki = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         public bool czyPoReflektorze = false;
 
+        //Konstruktor
         public Rotor(string rType, int roPos, int riPos)
         {
             rotorType = rType;
             rotorPos = roPos;
             ringPos = riPos;
-            offset = rotorPos - 1 + ringPos - 1;
+            offset = (rotorPos - 1) - (ringPos - 1);
 
             if (rotorType == "I")
             {
@@ -106,14 +114,18 @@ namespace hackaton_teamchleb
             }
         }
 
+        //Zmienia znak zgodnie z obecnym ustawieniem rotora i jego pierścienia
         public char ZamienZnak(char ch)
         {
-            if (znaki.IndexOf(ch) + offset < 26)
+            if (znaki.IndexOf(ch) + offset < 26 && znaki.IndexOf(ch) + offset >= 0)
             {
                 ch = rotorKey[znaki.IndexOf(ch) + offset];
             } else if (znaki.IndexOf(ch) + offset >= 26)
             {
                 ch = rotorKey[znaki.IndexOf(ch) + offset - 26];
+            } else if (znaki.IndexOf(ch) + offset < 0)
+            {
+                ch = rotorKey[znaki.IndexOf(ch) + offset + 26];
             }
                 
             return ch;
@@ -122,14 +134,17 @@ namespace hackaton_teamchleb
 
     class Rotors
     {
+        //Przechowuje listę z wszystkimi rotorami i odpowiada za ich obrót
         //Pierwszy rotor w liście to ma być rotor prawy, drugi to rotor środkowy, a trzeci to rotor lewy
         List<Rotor> rotory = new List<Rotor>();
 
+        //Konstruktor
         public Rotors(List<Rotor> rs)
         {
             rotory = rs;
         }
 
+        //Obraca rotory
         public void ObrocRotory()
         {
             rotory[0].rotorPos++;
@@ -139,7 +154,6 @@ namespace hackaton_teamchleb
                 if (rotory[1].rotorKey[rotory[1].rotorPos] == rotory[1].rotorRotateChar)
                 {
                     rotory[2].rotorPos++;
-                    //Dokończyć
                 }
             }
         }
@@ -147,9 +161,11 @@ namespace hackaton_teamchleb
 
     class EntryWheel
     {
+        //Zamienia znaki zgodnie z alfabetem na znaki zgodnie z klawiaturą QWERTZ
         string znaki = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         string klucz = "QWERTZUIOPASDFGHJKLYXCVBNM";
 
+        //Robi dokładnie co w komentarzu powyżej
         public char ZamienZnak(char ch)
         {
             ch = klucz[znaki.IndexOf(ch)];
@@ -159,10 +175,12 @@ namespace hackaton_teamchleb
 
     class Reflector
     {
+        //Zamienia znaki na inne zgodnie z kluczem, które są później przekazane z powrotem do rotorów
         string reflectorType;
         string reflectorKey;
         string znaki = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+        //Konstruktor
         public Reflector(string rType)
         {
             reflectorType = rType;
@@ -176,6 +194,7 @@ namespace hackaton_teamchleb
             }
         }
 
+        //Zmienia znak jak napisano powyżej
         public char ZamienZnak(char ch)
         {
             ch = reflectorKey[znaki.IndexOf(ch)];
