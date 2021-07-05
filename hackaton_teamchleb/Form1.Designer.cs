94688088d057afa0e949728f76bdb3e980f08c55
ChrisKJM <chriskjm0903@gmail.com>
2021-07-05 16:04:17 +0200

    changed border style to fixed single

--- hackaton_teamchleb/Form1.Designer.cs
+++ hackaton_teamchleb/Form1.Designer.cs
@@ -1115,6 +1115,7 @@
             this.Controls.Add(this.input_box);
             this.Controls.Add(this.output_box);
             this.Controls.Add(this.code_button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
